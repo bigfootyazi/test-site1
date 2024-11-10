df71c3b3196545ffbdbffa8522c406051e9a6ddc
bigfootyazi <1023248044@qq.com>
2024-11-10 15:00:43 +0800

    more info

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,31 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-alert("hello!");
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images\firefox.jpg") {
+    myImage.setAttribute("src", "images\edge.jpg");
+  } else {
+    myImage.setAttribute("src", "images\firefox.jpg");
+  }
+};
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+
+
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+
+  myButton.onclick = () => {
+    setUserName();
+  };
+
